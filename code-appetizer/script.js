3504c43002d623cd1476d1e459533a37d1b86fdb
adynata <clairewoods@gmail.com>
2015-09-14 20:43:30 -0700

    first commit

--- code-appetizer/script.js
+++ code-appetizer/script.js
@@ -3,3 +3,22 @@
   Your Javascript here
 
 */
+
+var addToFooDiv = function() {
+    var currentValue = textInput[0].value;
+    var allValues = [];
+    allValues.push(currentValue);
+    var divContents = document.createTextNode;
+    divContents = allValues.join(", ");
+    var foo = document.getElementsByClassName("foo")[0]
+    foo[0].appendChild(divContents);
+    console.log(allValues);
+    textInput[0].value = "";
+
+};
+
+var textInput = document.getElementsByClassName("text-input");
+
+document.getElementsByClassName("submit-button")[0].addEventListener('click', addToFooDiv);
+
+console.log(document.getElementsByClassName("submit-button"));
--- exercise-1/script.js
+++ exercise-1/script.js
@@ -1,6 +1,31 @@
-jQuery(function(){
+// jQuery(function(){
+$("article").hide();
+        
 
-  // Your javascript code here
+// });
 
+// var hideArticle = $("article").hide();
+
+
+// var home = $("#home").show();
+
+$(document).ready(function(){
+    $("a").click(function() {
+            $("article").hide();
+            $("a").css("background", "#fafafa");
+            $(this).css("background", "#aaa");
+            var navLink = $(this).attr("href");
+            console.log("article"+navLink);
+            // $("article").transition({
+            //     perspective: '100px',
+            //     rotate3d: '1,1,0,180deg'
+            // });
+            $("article"+navLink).show();
+            $("article").transition({
+                perspective: '1000px',
+                rotate3d: '1,1,0,180deg'
+            });
+    });
 });
+
 
