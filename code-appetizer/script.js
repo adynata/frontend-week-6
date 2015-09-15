352711f0e4d716f715583ad41ccd632262a0884b
adynata <clairewoods@gmail.com>
2015-09-15 15:13:14 -0700

    code appetizer rework

--- code-appetizer/script.js
+++ code-appetizer/script.js
@@ -3,28 +3,46 @@
   Your Javascript here
 
 */
+var allValues = [];
 
-// var addToFooDiv = function() {
-//     var currentValue = textInput[0].value;
-//     var allValues = [];
-//     allValues.push(currentValue);
-//     var divContents = document.createTextNode;
-//     divContents = allValues.join(", ");
-//     var foo = document.getElementById("foo");
-//     console.log(foo);
-//     foo.appendChild(divContents);
-//     console.log(allValues);
-//     textInput[0].value = "";
 
-// };
+var addToFooDiv = function() {
+    var currentValue = textInput[0].value;
+    allValues.push(currentValue);
+    var divContents = document.createTextNode;
+    divContents = allValues.join(", ");
+    var foo = document.getElementById("foo");
+    console.log(foo);
+    foo.innerText = divContents;
+    console.log(allValues);
+    textInput[0].value = "";
+    changeColor();
 
-// var textInput = document.getElementsByClassName("text-input");
+};
 
-// document.getElementsByClassName("submit-button")[0].addEventListener('click', addToFooDiv);
 
-// console.log(document.getElementsByClassName("submit-button"));
+function changeColor() {
+  var foo = document.getElementById("foo");
+  foo.style.background = bgColors[1];
+  console.log("the current bg color is " + foo.style.background);
+  console.log("first index place is " + bgColors[0]);
 
-$("button").click(function() {
-    var inputValue = $("text-input").value;
-    $("#foo").append("<p>"+inputValue+"</p>");
-}); 
+  shiftColors();
+}
+
+function shiftColors() {
+  var shiftedColor = bgColors.shift(1);
+  console.log(shiftedColor);
+  console.log(bgColors);
+  bgColors.push(shiftedColor);
+  console.log(bgColors);
+};
+
+var bgColors = ["red", "cadetblue", "#3f3f3f"];
+
+
+var textInput = document.getElementsByClassName("text-input");
+
+document.getElementsByClassName("submit-button")[0].addEventListener('click', addToFooDiv);
+
+console.log(document.getElementsByClassName("submit-button"));
